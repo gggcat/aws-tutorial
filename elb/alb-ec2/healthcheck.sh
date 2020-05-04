7044a4a2f153ea0503691422ed3e0b4131fff7ba
gggcat <61681628+gggcat@users.noreply.github.com>
2020-05-05 04:21:23 +0900

    ALB+EC2構成のチュートリアル

---
+++ elb/alb-ec2/healthcheck.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TARGET_GROUP_ARN=$(aws elbv2 describe-target-groups | jq -r '.TargetGroups[] | select (.TargetGroupName == "tutorial-alb-ec2-tg") | .TargetGroupArn')
+
+aws elbv2 describe-target-health --target-group-arn ${TARGET_GROUP_ARN}
